76fec1bd319c423c1206f4e84ac2737facc4325c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-18 17:40:43 +0530

    Enhancements (#3)

--- src/script.js
+++ src/script.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const generateNumber = () => {
         const randomColor = `#${Math.random().toString(16).substring(2, 8)}`;
         document.body.style.backgroundColor = randomColor;
-
         hexcolor.textContent = randomColor;
     };
 
     const resetNumber = () => {
         hexcolor.textContent = "#ffffff";
+        document.body.style.backgroundColor = "#ffffff";
     };
 
     generateBtn.addEventListener("click", generateNumber);
